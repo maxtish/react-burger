052816776b72a3d8ea15cfed90a0f6321c95b380
Max Tishenko <rd-max@ya.ru>
2022-08-16 20:30:48 +0300

    add api fetch

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -5,18 +5,44 @@ import AppHeader from '../appheader/appheader';
 import BurgerIngredients from '../burgeringredients/burgeringredients';
 import BurgerConstructor from '../burgerconstructor/burgerconstructor';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className={AppStyles.page}>
-        <AppHeader />
+function App() {
+  const [state, setState] = React.useState({
+    isLoading: false,
+    hasError: false,
+    data: [],
+  });
+
+  React.useEffect(() => {
+    const getIngredients = async () => {
+      setState({ ...state, hasError: false, isLoading: true });
+      const res = await fetch('https://norma.nomoreparties.space/api/ingredients');
+      const data = await res.json();
+      const arr = data.data;
+      setState({ ...state, data: arr, hasError: false, isLoading: false });
+    };
+    getIngredients();
+  }, []);
+  const { isLoading, hasError, data } = state;
+
+  console.log(data);
+
+  return (
+    <div className={AppStyles.page}>
+      <AppHeader />
+
+      {isLoading && 'Загрузка...'}
+      {hasError && 'Ошибка'}
+      {!isLoading && !hasError && !data.length && 'Ошибка - нет массива'}
+      {!isLoading && !hasError && data.length && (
         <div className={AppStyles.content}>
-          <BurgerIngredients dataIngredients={dataIngredients} />
+          <BurgerIngredients dataIngredients={data} />
           <BurgerConstructor dataIngredients={dataIngredients} />
         </div>
-      </div>
-    );
-  }
+      )}
+    </div>
+  );
 }
 
 export default App;
+/*{!isLoading && !hasError && <BurgerIngredients dataIngredients={data} />}
+        {!isLoading && !hasError && <BurgerConstructor dataIngredients={data} />}*/
